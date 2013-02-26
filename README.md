f25b5d42f8106cabcd88612b65c654924f462d1f
Andrew Mleczko <andrew@mleczko.net>
2013-02-26 08:42:08 +0100

    initial release

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-pyramid_alfresco
-================
+pyramid_alfresco README
+==================
 
-Pyramid Alfresco integratio
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $venv/bin/python setup.py develop
+
+- $venv/bin/initialize_pyramid_alfresco_db development.ini
+
+- $venv/bin/pserve development.ini
+
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'SQLAlchemy',
+    'transaction',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='pyramid_alfresco',
+      version='0.0',
+      description='pyramid_alfresco',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='pyramid_alfresco',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = pyramid_alfresco:main
+      [console_scripts]
+      initialize_pyramid_alfresco_db = pyramid_alfresco.scripts.initializedb:main
+      """,
+      )
