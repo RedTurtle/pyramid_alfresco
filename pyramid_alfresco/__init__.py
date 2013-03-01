454b86491325421e26a22a7a51fc25719a2e2a53
Andrew Mleczko <andrew@mleczko.net>
2013-03-01 17:55:12 +0100

    cleanup

--- pyramid_alfresco/__init__.py
+++ pyramid_alfresco/__init__.py
@@ -1,28 +1,41 @@
 from pyramid.config import Configurator
-from sqlalchemy import engine_from_config
 from pyramid_beaker import session_factory_from_settings
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+from pyramid.security import Authenticated
+from pyramid.security import Allow
 
-from .models import (
-    DBSession,
-    Base,
-    )
+
+class Root(object):
+    __acl__ = [(Allow, Authenticated, 'view'),]
+    def __init__(self, request):
+        self.request = request
+
+
+def groupfinder(userid, request):
+    return []
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    engine = engine_from_config(settings, 'sqlalchemy.')
-    DBSession.configure(bind=engine)
-    Base.metadata.bind = engine
     session_factory = session_factory_from_settings(settings)
-    config = Configurator(settings=settings)
+    authn_policy = AuthTktAuthenticationPolicy('supersecret',
+                                               callback=groupfinder)
+    authz_policy = ACLAuthorizationPolicy()
+    config = Configurator(settings=settings,
+                          authentication_policy=authn_policy,
+                          authorization_policy=authz_policy,
+                          root_factory=Root)
     config.set_session_factory(session_factory)
     config.include('pyramid_fanstatic')
     config.include('pyramid_beaker')
     config.include('pyramid_alfresco.oauth')
     config.add_alfresco_login_from_settings(prefix='alfresco.')
     config.add_static_view('static', 'static', cache_max_age=3600)
+
     config.add_route('home', '/')
+    config.add_route('login', '/login')
     config.add_route('oauth', '/alfresco_oauth')
     config.scan()
     return config.make_wsgi_app()
--- pyramid_alfresco/views.py
+++ pyramid_alfresco/views.py
@@ -1,25 +1,43 @@
-import json
+#import json
+from pyramid.security import remember
+from pyramid.security import authenticated_userid
+from pyramid.httpexceptions import HTTPFound
+from pyramid.httpexceptions import HTTPForbidden
+from pyramid.url import route_url
 from pyramid.view import view_config
 from velruse import login_url
 
 from .resources import pyramid_alfresco
 
 
-@view_config(name='login', renderer='templates/sign.pt')
+@view_config(route_name='home', renderer='templates/welcome.pt', permission='view')
 def welcome(request):
     pyramid_alfresco.need()
     return {'login_url': login_url}
 
-@view_config(
-    context='velruse.AuthenticationComplete',
-    renderer='templates/welcome.pt')
-def login_complete_view(request):
+
+@view_config(context=HTTPForbidden)
+def forbidden_view(request):
+    # do not allow a user to login if they are already logged in
+    if authenticated_userid(request):
+        return HTTPForbidden()
+    loc = route_url('login', request, _query=(('next', request.path),))
+    return HTTPFound(location=loc)
+
+
+@view_config(route_name='login', renderer='templates/sign.pt')
+def login_view(request):
     pyramid_alfresco.need()
-    context = request.context
-    result = {
-        'profile': context.profile,
-        'credentials': context.credentials,
-    }
-    return {
-        'result': json.dumps(result, indent=4),
-    }
+    return {'login_url': login_url}
+
+
+@view_config(context='velruse.AuthenticationComplete')
+def login_complete_view(request):
+    #pyramid_alfresco.need()
+    #context = request.context
+    #result = {
+    #    'profile': context.profile,
+    #    'credentials': context.credentials,
+    #}
+    headers = remember(request, 'user')
+    return HTTPFound(location=route_url('home', request), headers=headers)
